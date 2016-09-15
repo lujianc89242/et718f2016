7790fde87b4577523a67940c3f6b4fee62b44f72
JiancongLu <rogerfrontend@gmail.com>
2016-09-15 12:54:29 -0400

    failed to create articles

---
+++ blog/test/controllers/aticles_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class AticlesControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ blog/test/helpers/aticles_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class AticlesHelperTest < ActionView::TestCase
+end
